e68425724688e3762a44fe76a52b3807cd1b22a8
Robert Reinhard <robert.reinhard@bukwild.com>
2023-06-05 13:07:57 -0700

    Use SmartLink in PortableText

--- nuxt-app/components/globals/portableText/PortableTextBase.js
+++ nuxt-app/components/globals/portableText/PortableTextBase.js
@@ -1,3 +1,6 @@
+// Import SmartLink explicitly
+import SmartLink from 'vue-routing-anchor-parser/smart-link'
+
 // Render a @nuxtjs/sanity instance with our customizations
 export default {
   functional: true,
@@ -17,10 +20,32 @@ export default {
 
         // Using "value" for prop to match @portabletext/react
         blocks: props.value,
+
+        // Wire up custom components
+        serializers: {
+          marks: {
+            link: makeLinkSerializer(),
+          }
+        }
       },
 
       // Prepend Sanity/typography prose base classes
       class: ['prose prose-base', data.class]
     })
+  }
+}
+
+// Use Smart Link for anchor links by making a functional component that maps
+// the portable text link data to what SmartLink is expecting.
+function makeLinkSerializer() {
+  return {
+    functional: true,
+    render(create, { props, children }) {
+      return create(SmartLink, {
+        props: {
+          to: props.href, // Sanity stores the link as "href"
+        }
+      }, children)
+    }
   }
 }
