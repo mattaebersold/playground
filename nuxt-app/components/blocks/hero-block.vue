6b3de90eb85b9ca9053651a7aa6207820ac0e146
Robert Reinhard <robert.reinhard@bukwild.com>
2023-06-07 14:12:11 -0700

    Put background behind the body text

--- nuxt-app/components/blocks/hero-block.vue
+++ nuxt-app/components/blocks/hero-block.vue
@@ -1,15 +1,18 @@
 <template>
 
-<div>
+<div class='relative isolate overflow-hidden pt-header bg-slate-900'>
+
+    <!-- Example of rendering an image -->
+  <SanityVisual :image='background' sizes='100vw' expand />
 
   <!-- Render body text -->
-  <div class='max-w-screen-md mx-auto px-gutter'>
-    <h2>Hero block</h2>
+  <div class='
+    relative
+    max-w-screen-md mx-auto px-gutter
+    py-32f md:py-56f
+    text-center text-white'>
     <portable-text-marketing :value='body' />
   </div>
-
-  <!-- Example of rendering an image -->
-  <SanityVisual :image='background' />
 
 </div>
 
