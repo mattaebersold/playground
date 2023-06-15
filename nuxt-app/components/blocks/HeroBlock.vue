1479c01484a3b91257d3bbeb66d78b55abc8bcec
Matthew Aebersold <matt.aebersold@bkwld.com>
2023-06-15 14:33:27 -0700

    Added animations to the hero block

--- nuxt-app/components/blocks/HeroBlock.vue
+++ nuxt-app/components/blocks/HeroBlock.vue
@@ -2,17 +2,21 @@
 
 <div class='relative isolate overflow-hidden pt-header bg-slate-900'>
 
-    <!-- Example of rendering an image -->
-  <SanityVisual :image='background' sizes='100vw' expand />
+  <in-view animate once class='animate-[scale_.75s_0s_both] absolute w-full h-full top-0'>
+    <SanityVisual :image='background' sizes='100vw' expand class='absolute inset-0' />
+  </in-view>
+
 
   <!-- Render body text -->
-  <div class='
-    relative
-    max-w-screen-md mx-auto px-gutter
-    py-32f md:py-56f
-    text-center text-white'>
-    <portable-text-marketing :value='body' />
-  </div>
+  <in-view animate once class='animate-[scale_.75s_.3s_both]'>
+    <div class='
+      relative
+      max-w-screen-md mx-auto px-gutter
+      py-32f md:py-56f
+      text-center text-white'>
+      <portable-text-marketing :value='body' />
+    </div>
+  </in-view>
 
 </div>
 
