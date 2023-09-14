a8ad6f5ab459bd598c44781b2aa6481be90fd695
Isaaz Garcia <isaazgarciawork@gmail.com>
2023-09-14 01:09:59 -0700

    Updating z index

--- nuxt-app/components/globals/preview-controls.vue
+++ nuxt-app/components/globals/preview-controls.vue
@@ -3,7 +3,7 @@
     <client-only>
       <div
         v-if="$nuxt.isPreview"
-        class="fixed bottom-5 left-5 p-2 text-left bg-black text-white bg-opacity-50 flex flex-col"
+        class="fixed bottom-5 z-50 left-5 p-2 text-left bg-black text-white bg-opacity-50 flex flex-col"
       >
           <button class="font-semibold underline text-left text-sm" @click="$nuxt.refresh()">
             Refresh Preview
